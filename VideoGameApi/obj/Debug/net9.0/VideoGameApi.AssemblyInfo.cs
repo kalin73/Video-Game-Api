110463ef414a010ec88a8058e58509b0aa2308e6
kalin73 <104739268+kalin73@users.noreply.github.com>
2025-06-06 12:41:07 +0300

    adding mongodb dependency

--- VideoGameApi/obj/Debug/net9.0/VideoGameApi.AssemblyInfo.cs
+++ VideoGameApi/obj/Debug/net9.0/VideoGameApi.AssemblyInfo.cs
@@ -25,7 +25,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VideoGameApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+facfdf2258b1cc762d9dfccd1cabe2bccc4355b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ebd0f3061aa6adaa67bf8b9494df8185b3c318b")]
 [assembly: System.Reflection.AssemblyProductAttribute("VideoGameApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VideoGameApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
