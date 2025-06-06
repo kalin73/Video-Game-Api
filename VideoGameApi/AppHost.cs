1a92e4c785c791f9f492a75e62f9ca493258e57f
kalin73 <104739268+kalin73@users.noreply.github.com>
2025-06-06 19:00:19 +0300

    adding db connections with env variables

--- VideoGameApi/AppHost.cs
+++ VideoGameApi/AppHost.cs
@@ -1,7 +1,23 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using VideoGameApi.Config;
+using VideoGameApi.Services;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<VideoGameService>();
+
+MongoDBSettings? mongoDbSettings = builder.Configuration.GetSection("MongoDBSettings").Get<MongoDBSettings>();
+builder.Services.Configure<MongoDBSettings>(builder.Configuration.GetSection("MongoDBSettings"));
+
+String? username = Environment.GetEnvironmentVariable("username");
+String? password = Environment.GetEnvironmentVariable("password");
+String url = $"mongodb+srv://{username}:{password}@mongodb.ldc9z5l.mongodb.net/?retryWrites=true&w=majority";
+
+builder.Services.AddDbContext<VideoGameDbContext>(options => options
+    .UseMongoDB(url, mongoDbSettings.DatabaseName ?? ""));
 
 builder.Services.AddControllers();
 
