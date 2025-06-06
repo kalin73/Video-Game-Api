954ec8858cf71f5ec569413f2ef6917dff9e72f1
kalin73 <104739268+kalin73@users.noreply.github.com>
2025-06-06 19:43:03 +0300

    adding db context

---
+++ VideoGameApi/Services/VideoGameDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VideoGameApi.model;
+
+namespace VideoGameApi.Services;
+
+public class VideoGameDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<VideoGame> Games { get; init; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<VideoGame>();
+    }
+}
