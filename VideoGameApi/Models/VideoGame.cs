c25c35a3992ea1937afba7ae54528796dbd68d6b
kalin73 <104739268+kalin73@users.noreply.github.com>
2025-06-06 12:41:26 +0300

    refactoring

--- VideoGameApi/Models/VideoGame.cs
+++ VideoGameApi/Models/VideoGame.cs
@@ -1,8 +1,12 @@
-﻿namespace VideoGameApi.model;
+﻿using MongoDB.Bson;
+using MongoDB.EntityFrameworkCore;
 
+namespace VideoGameApi.model;
+
+[Collection("games")]
 public class VideoGame
 {
-    public int Id { get; set; }
+    public ObjectId Id { get; set; }
     public string? Title { get; set; }
     public string? Platform { get; set; }
     public string? Developer { get; set; }
